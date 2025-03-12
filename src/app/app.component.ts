0fe23aeb53789c60e14e5ac9110b1f1e452750a8
Your Name <you@example.com>
2025-03-11 21:23:27 -0300

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CommonModule, RouterOutlet],
+  template: `<router-outlet></router-outlet>`,
 })
-export class AppComponent {
-  title = 'curso-angular-portfolio';
-}
+export class AppComponent {}
