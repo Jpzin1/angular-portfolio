0f73eac7750679e6affa9ac564cca98530628f8f
Your Name <you@example.com>
2025-03-13 16:06:37 -0300

    new commit scss, html. ts

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,16 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 
 // Interface
 import { IProjects } from '../../interface/IProjects.interface';
 
+// Material
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+
+// Enum
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+
+// Dialog
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
+
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-  public arrayProjects = signal<[IProjects]>([
+  #dialog = inject(MatDialog);
+  public arrayProjects = signal<IProjects[]>([
     {
       src: 'assets/img/projects/vfull.png',
       alt: 'Projeto Vida Fullstack',
@@ -26,4 +36,11 @@ export class ProjectsComponent {
       ],
     },
   ]);
+
+  public openDialog(data: IProjects) {
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS,
+    })
+  }
 }
