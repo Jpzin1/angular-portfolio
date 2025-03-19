bc66d0e42504d24dad6aa4539a9dc35f68eeb03f
Jpzin1 <joaojanico@sempreceub.com>
2025-03-19 08:19:10 -0300

    new commit atualizando sobre mim

--- src/app/modules/portfolio/components/experiences/experiences.component.ts
+++ src/app/modules/portfolio/components/experiences/experiences.component.ts
@@ -14,45 +14,31 @@ export class ExperiencesComponent {
   public arrayExperiences = signal<IExperiences[]>([
     {
       summary: {
-        strong: 'Front-end Especialista',
-        p: 'ZUP | Set 2020 - Present',
+        strong: 'Introdução pessoal',
+        p: 'Um pouco de mim',
       },
-      text: '<p>Desde setembro de 2020, atuo como Analista de Sistema Especialista na ZUP, destacando-me nas áreas Front-end, utilizando Html5, Css3, Javascript, TypeScript, Angular 2+, Angular Material e NgRx, e Back-end, onde o NodeJs é aplicado em Provas de Conceito, caso necessário.</p> <p>Além disso, minha experiência abrange a integração com Micro Serviços, Docker, GitLab, Jira e Scrum. Atualmente, enfrento o desafio significativo de contribuir para o desenvolvimento do Open Finance para o Banco Itaú, alcançando sucesso ao implementar a ferramenta em conformidade com os marcos regulatórios.</p> <p>Destaco também a criação de componentes altamente reutilizáveis e personalizáveis no front-end, utilizando Angular, como parte do nosso compromisso com a eficiência e a qualidade do código.</p>',
+      text: '<p>Me chamo João Pedro, tenho 20 anos e sou estudante de Ciências da computação. Atualmente estou cursando o quinto semestre e estou em busca da minha primeira experiência profissional. </p>',
     },
     {
       summary: {
-        strong: 'Fullstack Developer',
-        p: 'Everis | Out 2019 - set 2020',
+        strong: 'Minhas habilidades',
+        p: 'Um pouco mais a fundo das minhas habilidades',
       },
-      text: '<p>Durante meu período na Everis de outubro de 2019 a setembro de 2020, atuei como Analista de Sistema, liderando e desenvolvendo equipes.</p> <p>Minhas responsabilidades no front-end incluíram o uso de Html5, Css3, Javascript, TypeScript, Angular, Angular Material e NgRx, enquanto no back-end, trabalhei com NodeJs (Express/NestJs), Sequelize/TypeOrm, MongoDb, MariaDb e Redis.</p> <p>Além disso, liderei integrações com Micro Serviços, Jenkins, Gcloud, Kubernetes e Docker, utilizando ferramentas como GitLab, Jira e Scrum.</p> <p>Durante esse período, enfrentei e superei desafios significativos, destacando-se o desenvolvimento de um ChatBot.</p> <p>Como resultado, fui promovido a Líder da Squad Fullstack Angular/NodeJs, conduzi melhorias contínuas em um sistema legado e aprimorei minhas habilidades nos processos do GitFlow.</p>',
+      text: '<p>Atualmente, estou focado na área de full-stack, onde desenvolvo e aprimoro minhas habilidades em diversas linguagens e tecnologias. Tenho experiência em tecnologias específicas, como (JavaScript, Angular, node.js, etc) e sou apaixonado por usar essas ferramentas para resolver desafios complexos e criar soluções inovadoras. Minha abordagem é sempre voltada para a integração eficiente entre front-end e back-end, garantindo uma experiência completa e funcional para os usuários. Estou sempre buscando aprender e me atualizar para entregar soluções de qualidade e que atendam às necessidades reais dos projetos.</p>',
     },
     {
       summary: {
-        strong: 'Consultor Web',
-        p: 'Consultor Web | Set 2018 - Set 2019',
+        strong: 'Objetivos e Visões futuras',
+        p: 'Meus objetivos',
       },
-      text: '<p>Em minha jornada como consultor web, tive a oportunidade de ser a ponte para várias empresas embarcarem no universo online.</p> <p>Por meio de projetos cuidadosamente elaborados, explorei o vasto espectro do desenvolvimento front-end, utilizando Html5, Css3, Javascript, TypeScript, Angular e Angular Material.</p> <p>Nos bastidores, no back-end, construí robustas infraestruturas com NodeJs (Express/NestJs), Sequelize/TypeOrm e integrações eficientes com bancos de dados MySql/Postgress.</p>',
+      text: '<p>Estou sempre em busca de aprender mais e evoluir, principalmente na área de desenvolvimento web. Meu objetivo é continuar me aprimorando cada vez mais nos meus conhecimentos de desenvolvimento web, explorando novos caminhos e sempre buscando o melhor em tudo que faço.</p>',
     },
     {
       summary: {
-        strong: 'Fullstack Developer',
-        p: 'MKTEW21 |  Set 2016 - Set 2018',
+        strong: 'Toque Pessoal e Criativo',
+        p: 'Um pouco do meu hobby',
       },
-      text: '<p>Minha jornada na empresa mktew21 foi um verdadeiro mergulho no mundo fullstack, onde absorvi conhecimentos em diversas tecnologias.</p> <p>Foi um período de aprendizado intenso no desenvolvimento end-to-end, abrangendo a criação de aplicativos híbridos, desktop e web.</p> <p>E nesse cenário dinâmico que tomei a decisão de direcionar meu foco para as tecnologias NodeJs, TypeScript e Angular.</p> <p>No front-end, explorei as ferramentas Html5, Css3, Javascript, TypeScript, Angular e Angular Material, enquanto nos bastidores, no back-end, estive envolvido com NodeJs (Express), Sequelize, Laravel (PHP) e integrações com bancos de dados MySql/Postgress.</p>',
-    },
-    {
-      summary: {
-        strong: 'Decolar.com Front-end',
-        p: 'Decolar.com | Set 2014 - Set 2016',
-      },
-      text: '<p>Durante minha passagem pelo Decolar.com, dediquei-me intensamente à criação e atualização de páginas de vendas, além de colaborar com o departamento de Recursos Humanos na implementação de um sistema de gestão de vagas.</p> <p>Extendendo minha contribuição a diversos setores, como marketing, vendas, entre outros, empreguei as tecnologias da época no desenvolvimento, incluindo Html5, Css3, Javascript, Jquery e AngularJS no front-end, e PHP no back-end.</p>',
-    },
-    {
-      summary: {
-        strong: 'EW21 Front-end',
-        p: 'EW21 | Mar 2013 - Set 2014',
-      },
-      text: '<p>No ew21, marco inicial da minha trajetória profissional, mergulhei em um aprendizado abrangente que englobava não apenas front-end, design e backend, mas também incluía domínio em tecnologias como Flash e muito mais.</p> <p>Essa empresa se revelou uma verdadeira escola inicial, onde absorvi conhecimentos variados. No front-end, desenvolvi habilidades com Html5, Css3, Javascript, Jquery e Wordpress, enquanto no back-end, explorei PHP, Cake (PHP) e integrações com bancos de dados MySql.</p>',
+      text: '<p>Quando não estou estudando, gosto de jogar videogames. Essa atividade me permite relaxar e descontrair, ajudando a clarear a mente para que eu possa voltar ao foco com mais disposição e criatividade.</p>',
     },
   ]);
 }
